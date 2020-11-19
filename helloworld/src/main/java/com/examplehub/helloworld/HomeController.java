24b7c66dfcf414ada07eb2d9a8f3ade90869c624
shellhub <shellhub.me@gmail.com>
2020-11-19 10:28:59 +0800

    add test case

--- helloworld/src/main/java/com/examplehub/helloworld/HomeController.java
+++ helloworld/src/main/java/com/examplehub/helloworld/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Home {
+public class HomeController {
   @RequestMapping("/")
   public String home() {
     return "Hello World!";
