8436d72a3fa105695939a2612b8ae96fa4e873a3
shellhub <shellhub.me@gmail.com>
2020-11-19 09:39:40 +0800

    add banner

---
+++ custombanner/src/main/java/com/examplehub/custombanner/CustombannerApplication.java
@@ -0,0 +1,13 @@
+package com.examplehub.custombanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustombannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustombannerApplication.class, args);
+	}
+
+}
---
+++ custombanner/src/test/java/com/examplehub/custombanner/CustombannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplehub.custombanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustombannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
