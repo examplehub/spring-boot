a7a8450fd6dde59927915201277b63de174a2052
shellhub <shellhub.me@gmail.com>
2020-11-20 14:17:45 +0800

    custom port

---
+++ custom-port/src/main/java/com/examplehub/customport/CustomPortApplication.java
@@ -0,0 +1,13 @@
+package com.examplehub.customport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomPortApplication.class, args);
+	}
+
+}
