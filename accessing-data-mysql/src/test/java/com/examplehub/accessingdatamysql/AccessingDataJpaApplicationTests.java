e601c536f76a0b5c7a9f316ca557a38dbbeddb62
shellhub <shellhub.me@gmail.com>
2020-11-23 14:30:34 +0800

    access mysql jpa

---
+++ accessing-data-mysql/src/test/java/com/examplehub/accessingdatamysql/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplehub.accessingdatamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ consuming-rest/README.md
@@ -0,0 +1,6 @@
+To run this module. Run below command.
+``` shell
+mvn spring-boot:run
+curl localhost:8080/demo/add -d name=shellhub -d email=shellhub.me@gmail.com
+curl 'localhost:8080/demo/all'
+```
