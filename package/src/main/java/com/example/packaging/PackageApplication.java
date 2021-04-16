f75bc651f5abec23ef76d21eecf8be87b0184b33
Du Yuanchao <shellhub.me@gmail.com>
2021-04-16 18:39:03 +0800

    package spring boot application (#3)
    * package spring boot application
    
    * ignore consuming-rest build
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ package/src/main/java/com/example/packaging/PackageApplication.java
@@ -0,0 +1,12 @@
+package com.example.packaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PackageApplication.class, args);
+  }
+}
---
+++ package/src/test/java/com/example/packaging/PackageApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.packaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
